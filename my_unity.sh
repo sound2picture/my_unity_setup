59e6f2a9b095d33822f9b705a65dfdce5bb596d9
sound2picture <sound2picture@users.noreply.github.com>
2017-05-22 00:54:26 +0300

    Create my_unity.sh

---
+++ my_unity.sh
@@ -0,0 +1,19 @@
+########### my unity setup
+#
+#
+########### SYSTEM
+#
+sudo apt install git
+
+
+########### INTERFACE
+sudo apt install unity-tweak-tool
+
+
+########### MEDIA
+sudo apt install vlc
+sudo apt install ffmpeg
+sudo apt install mediainfo mediainfo-gui
+
+########### APPLICATIONS
+sudo apt install ubuntu-mate-welcome
