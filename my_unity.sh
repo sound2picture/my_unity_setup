10a661984c2f0fe98964784e345695a5d826452d
sound2picture <sound2picture@users.noreply.github.com>
2017-05-22 01:06:35 +0300

    Update my_unity.sh

--- my_unity.sh
+++ my_unity.sh
@@ -1,14 +1,23 @@
 ########### my unity setup
-#
+# As of 2017-05-22
 #
 ########### SYSTEM
 #
+sudo apt update
+sudo apt upgrade
+# system tools
 sudo apt install git
-
+sudo apt install powertop
+sudo apt install htop
+sudo apt install terminix
 
 ########### INTERFACE
 sudo apt install unity-tweak-tool
-
+mkdir ~/.themes
+git clone https://github.com/LinxGem33/OSX-Arc-Plus.git
+mkdir ~/.icons
+cd ~/.icons
+git clone https://github.com/keeferrourke/la-capitaine-icon-theme.git
 
 ########### MEDIA
 sudo apt install vlc
@@ -17,3 +26,14 @@ sudo apt install mediainfo mediainfo-gui
 
 ########### APPLICATIONS
 sudo apt install ubuntu-mate-welcome
+sudo apt install darktable
+# GIMP 2.9
+sudo add-apt-repository ppa:otto-kesselgulasch/gimp-edge
+sudo apt-get update
+sudo apt-get install gimp
+
+
+# TODO
+# syncthing
+# pycharm
+# gimp 2.9
